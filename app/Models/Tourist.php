d18374b9d42f0351686bb07c2a622337543968c8
limancheng <1073517446@qq.com>
2019-10-05 20:06:36 +0800

    更新数据

--- app/Models/Tourist.php
+++ app/Models/Tourist.php
@@ -15,23 +15,7 @@ class Tourist extends BasicModel
     public $timestamps = false;
 
     public function recordRourist(){
-        die;
-        $macAddr = new MacAddr();
-        $mac_addr = $macAddr->GetMacAddr(PHP_OS);
-
-        $info = $this->where('mac_addr','=',$mac_addr)->first();
-        if(empty($info)){
-            $data = [
-                'mac_addr' => $mac_addr,
-                'ip' => '/',
-                'avatar' => '/img/vistor_'.rand(0,6).'.jpg',
-                'add_time' => date('Y-m-d H:i:s'),
-                'last_time'=> date('Y-m-d H:i:s'),
-            ];
-            $this::query()->insert($data);
-        }else{
-            $this::query()->where('mac_addr','=',$mac_addr)->update(['last_time'=>date('Y-m-d H:i:s')]);
-        }
+        //self::toSave();
 
     }
 
