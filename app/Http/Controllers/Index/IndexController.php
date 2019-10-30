29ed9258029376dd06b1524f59bed8343a5c775b
limancheng <limancheng@smartfinancecloud.com>
2019-10-30 10:11:22 +0800

    swoole测试

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -23,21 +23,7 @@ class IndexController extends Controller
     }
 
     public function swoole(){
-        $server = new swoole_websocket_server("0.0.0.0", 9501);
-
-        $server->on('open', function (swoole_websocket_server $server, $request) {
-            echo "server: handshake success with fd{$request->fd}\n";
-        });
-
-        $server->on('message', function (swoole_websocket_server $server, $frame) {
-            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-            $server->push($frame->fd, "this is server");
-        });
-
-        $server->on('close', function ($ser, $fd) {
-            echo "client {$fd} closed\n";
-        });
-
-        $server->start();
+        $ws = new \swoole_websocket_server("0.0.0.0", 9502);
+        $ws->push(1, "asfafaf");
     }
 }
